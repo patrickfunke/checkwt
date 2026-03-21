1d63cd144719a71205611de6d822842b4eaa571a
Leonardo Palomino <l.palomino1@icloud.com>
2026-03-21 10:59:41 +0100

    fix: copy buttons

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -107,7 +107,7 @@ export default function Home() {
                     <div className="text-xl font-bold flex justify-between items-center">
                             <div className="flex gap-2">
                             <div>JSON Web Token</div>
-                            {signatureValid !== null && <div className={(signatureValid === true ? "text-green-500 bg-green-500/10" : "text-red-500 bg-red-500/10") + " flex flex-row items-center gap-2 text-xs px-2 py-1 rounded-lg border"}>
+                            {(signatureValid !== null && tokenType !== 'JWE') && <div className={(signatureValid === true ? "text-green-500 bg-green-500/10" : "text-red-500 bg-red-500/10") + " flex flex-row items-center gap-2 text-xs px-2 py-1 rounded-lg border"}>
                                 {signatureValid === true ? <img className="w-2 h-2 inline" src="/correct.png" alt="Valid" /> : <img className="w-2 h-2 inline" src="/wrong.png" alt="Invalid" />}
                                 {signatureValid === true ? "Signature is valid" : `Signature is invalid`}
                             </div>
@@ -140,10 +140,10 @@ export default function Home() {
                 <div>
                     <div className="text-xl font-bold flex justify-between items-center">
                         <div>Decoded Header</div>
-                        <Button className="cursor-pointer" title="Copy" onClick={() => copyTextToClipboard(token)}>
+                        <Button className="cursor-pointer" title="Copy" onClick={() => copyTextToClipboard(JSON.stringify(header))}>
                             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 15H12C13.6569 15 15 13.6569 15 12V8C15 6.34315 13.6569 5 12 5H8C6.34315 5 5 6.34315 5 8V12C5 13.6569 6.34315 15 8 15Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M8 0.25C9.33915 0.25 10.5138 0.952094 11.177 2.00819C11.4679 2.47151 11.0737 3 10.5266 3C10.2123 3 9.93488 2.81318 9.7352 2.57055C9.32304 2.06973 8.6994 1.75 8 1.75H4C2.75736 1.75 1.75 2.75736 1.75 4V8C1.75 8.69927 2.06986 9.32232 2.57062 9.73428C2.81326 9.93389 3 10.2113 3 10.5255C3 11.0726 2.47146 11.4669 2.00808 11.176C0.952101 10.513 0.25 9.33902 0.25 8V4C0.25 1.92893 1.92893 0.25 4 0.25H8Z" fill="currentColor"></path></svg>
                         </Button>                    </div>
-                    <div className="bg-gray-50 dark:bg-[#0a1a3a] rounded-lg border border-gray-300 dark:border-[#1e1e1e] min-h-48 p-4">
+                    <div className="overflow-x-clip bg-gray-50 dark:bg-[#0a1a3a] rounded-lg border border-gray-300 dark:border-[#1e1e1e] min-h-48 p-4">
                         <PrettyPrint data={header} />
                     </div>
                 </div>
@@ -151,10 +151,10 @@ export default function Home() {
                 <div className="col-start-2">
                     <div className="text-xl font-bold flex justify-between items-center">
                         <div>Decoded Payload</div>
-                        <Button className="cursor-pointer" title="Copy" onClick={() => copyTextToClipboard(token)}>
+                        <Button className="cursor-pointer" title="Copy" onClick={() => copyTextToClipboard(JSON.stringify(payload))}>
                             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 15H12C13.6569 15 15 13.6569 15 12V8C15 6.34315 13.6569 5 12 5H8C6.34315 5 5 6.34315 5 8V12C5 13.6569 6.34315 15 8 15Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M8 0.25C9.33915 0.25 10.5138 0.952094 11.177 2.00819C11.4679 2.47151 11.0737 3 10.5266 3C10.2123 3 9.93488 2.81318 9.7352 2.57055C9.32304 2.06973 8.6994 1.75 8 1.75H4C2.75736 1.75 1.75 2.75736 1.75 4V8C1.75 8.69927 2.06986 9.32232 2.57062 9.73428C2.81326 9.93389 3 10.2113 3 10.5255C3 11.0726 2.47146 11.4669 2.00808 11.176C0.952101 10.513 0.25 9.33902 0.25 8V4C0.25 1.92893 1.92893 0.25 4 0.25H8Z" fill="currentColor"></path></svg>
                         </Button>                    </div>
-                    <div className="bg-gray-50 dark:bg-[#0a1a3a] rounded-lg border border-gray-300 dark:border-[#1e1e1e] min-h-48 p-4">
+                    <div className="overflow-x-clip bg-gray-50 dark:bg-[#0a1a3a] rounded-lg border border-gray-300 dark:border-[#1e1e1e] min-h-48 p-4">
                         <PrettyPrint data={payload} />
                     </div>
                 </div>
@@ -162,10 +162,10 @@ export default function Home() {
                 <div className="col-start-2">
                     <div className="text-xl font-bold flex justify-between items-center">
                         <div>Used Keys</div>
-                        <Button className="cursor-pointer" title="Copy" onClick={() => copyTextToClipboard(token)}>
+                        <Button className="cursor-pointer" title="Copy" onClick={() => copyTextToClipboard(JSON.stringify(usedKey))}>
                             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 15H12C13.6569 15 15 13.6569 15 12V8C15 6.34315 13.6569 5 12 5H8C6.34315 5 5 6.34315 5 8V12C5 13.6569 6.34315 15 8 15Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M8 0.25C9.33915 0.25 10.5138 0.952094 11.177 2.00819C11.4679 2.47151 11.0737 3 10.5266 3C10.2123 3 9.93488 2.81318 9.7352 2.57055C9.32304 2.06973 8.6994 1.75 8 1.75H4C2.75736 1.75 1.75 2.75736 1.75 4V8C1.75 8.69927 2.06986 9.32232 2.57062 9.73428C2.81326 9.93389 3 10.2113 3 10.5255C3 11.0726 2.47146 11.4669 2.00808 11.176C0.952101 10.513 0.25 9.33902 0.25 8V4C0.25 1.92893 1.92893 0.25 4 0.25H8Z" fill="currentColor"></path></svg>
                         </Button>                    </div>
-                    <div className="bg-gray-50 dark:bg-[#0a1a3a] rounded-lg border border-gray-300 dark:border-[#1e1e1e] min-h-48 p-4">
+                    <div className="overflow-x-clip bg-gray-50 dark:bg-[#0a1a3a] rounded-lg border border-gray-300 dark:border-[#1e1e1e] min-h-48 p-4">
                         <PrettyPrint data={usedKey} />
                     </div>
                 </div>
